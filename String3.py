4b7ce7575f702acfaad4316bd9d8e7b2c56581c8
reachbiju <boommen7@gmail.com>
2017-09-29 12:07:12 -0400

    Biju: Changes

--- String3.py
+++ String3.py
@@ -1,5 +1,4 @@
-
-members=['biju','rincy','nora']
+ members=['biju','rincy','nora']
 for item in range(len(members)):
     name=members[item]
     print("Happy Birthday: ", name)
---
+++ String4.py
@@ -0,0 +1,17 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+count=0
+tot=0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    pos1=line.find(":")
+    pos2=line.find("\n")
+    line2= float(line[pos1+1:pos2])
+    count=count+1
+    tot= tot+ line2
+#    print(line)
+#    print(line2)
+avg=tot/count
+avg1=str(avg)
+print("Average spam confidence: " + avg1)
+#print("Done")
